f3c8d5119bd77d28fd1d811d184edd33df00620a
Prince <banerjee.prince69@gmail.com>
2024-09-20 21:45:30 +0530

    feat: password encoding

--- Security/src/main/java/com/example/Security/service/UserService.java
+++ Security/src/main/java/com/example/Security/service/UserService.java
@@ -3,15 +3,19 @@ package com.example.Security.service;
 import com.example.Security.model.Users;
 import com.example.Security.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     @Autowired
     private UserRepo repo;
 
     public Users register(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
 
     }
