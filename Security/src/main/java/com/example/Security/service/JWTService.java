af71a863298f3172afccf12111e6e151b5cfc051
Prince <banerjee.prince69@gmail.com>
2024-09-21 01:29:18 +0530

    feat: generating token

--- Security/src/main/java/com/example/Security/service/JWTService.java
+++ Security/src/main/java/com/example/Security/service/JWTService.java
@@ -5,7 +5,11 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +17,17 @@ import java.util.Map;
 @Service
 public class JWTService {
 
-    private String secretKey = "cF781"
+    private String secretKey = "";
+
+    public JWTService(){
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGen.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public String generateToken(String username){
 
@@ -24,7 +38,7 @@ public class JWTService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() * 60 * 60 * 30))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
                 .and()
                 .signWith(getKey())
                 .compact();
